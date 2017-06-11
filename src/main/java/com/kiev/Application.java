48e05244dbb67b131edcae5ad8539c4b385e315b
evgeniya.opanasenko@outlook.com <63860DbnZ!>
2017-06-11 18:01:36 +0300

    JBOSS add

--- src/main/java/com/kiev/Application.java
+++ src/main/java/com/kiev/Application.java
@@ -1,16 +1,46 @@
 package com.kiev;
 
+import com.kiev.model.Item;
+import com.kiev.model.Stock;
+import com.kiev.repository.ItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication()
-public class Application {
+public class Application extends SpringBootServletInitializer {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    @Bean
+    public CommandLineRunner setup(ItemRepository repository) {
+        return (args) -> {
+            Item item1 = new Item(new Stock(123.5));
+            Item item2 = new Item(new Stock(334.5));
+            Item item3 = new Item(new Stock(445.8));
+            Item item4 = new Item(new Stock(445.8));
+            Item item5 = new Item(new Stock(445.8));
+            Item item6 = new Item(new Stock(445.8));
+
+            repository.save(item1);
+            repository.save(item2);
+            repository.save(item3);
+            repository.save(item4);
+
+            logger.info("The sample data has been generated");
+        };
+    }
 }
